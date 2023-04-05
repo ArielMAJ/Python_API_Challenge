803f19fecae3635aab8d11c0ca88c38bbcd32862
ArielMAJ <ariel.maj@hotmail.com>
2023-04-05 17:18:44 -0300

    [ADD/UPT][Issue #10] Add Route blueprint

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,3 +1,5 @@
 """Routes for the application."""
 from .partner import PARTNER_BLUEPRINT
 from .partners import PARTNERS_BLUEPRINT
+from .plant import PLANT_BLUEPRINT
+from .plants import PLANTS_BLUEPRINT
