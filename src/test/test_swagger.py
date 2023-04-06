f94c09c74b629825be50a6abe70bab4500a7dcba
ArielMAJ <ariel.maj@hotmail.com>
2023-04-06 17:02:56 -0300

    [ADD][Issue #6] Added tests to check if all routes are swagged.

---
+++ src/test/test_swagger.py
@@ -0,0 +1,36 @@
+"""Test the swagger spec"""
+
+import json
+import unittest
+import warnings
+
+from server import create_app
+import config
+
+
+class TestSwagger(unittest.TestCase):
+    """Test the swagger spec"""
+
+    @classmethod
+    def setUpClass(cls):
+        server = create_app(config.TestConfig)
+        cls.client = server.test_client()
+
+    def test_get_spec(self):
+        """The GET on /spec should return a 200"""
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", message="unclosed file")
+            response = self.client.get("/application/spec")
+        self.assertEqual(response.status_code, 200)
+
+    def test_swagger_is_not_empty(self):
+        """
+        The GET on /spec should return a dict with a non-empty paths property
+        """
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", message="unclosed file")
+            response = self.client.get("/application/spec")
+        response_json = json.loads(response.data.decode("utf-8"))
+        self.assertTrue(response_json["paths"])
