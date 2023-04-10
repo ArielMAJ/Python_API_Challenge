d85a7c67d010952eef7549d3738cdf5ac818a1ec
ArielMAJ <ariel.maj@hotmail.com>
2023-04-10 14:22:16 -0300

    [UPT][Issue #31] Added flask-cors to facilitate API being consumed by external clients.

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@
 from flasgger import Swagger
 from flask import Flask
 from flask.blueprints import Blueprint
+from flask_cors import CORS
 
 import routes
 from config import Config, DevelopmentConfig
@@ -47,6 +48,8 @@ if __name__ == "__main__":
     server = create_app(DevelopmentConfig)
     with server.app_context():
         db.create_all()
+    cors = CORS(server, resources={r"/*": {"origins": "*"}})
+
     print(f"Server running at http://{Config.HOST}:{Config.PORT}/")
     print(f"Swagger UI running at http://{Config.HOST}:{Config.PORT}/apidocs/")
     server.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
